3f22f7269d680e6bd8d46feea6e45d95b8cf8ad0
Yash Walia <48391286+ywalia01@users.noreply.github.com>
2020-09-02 10:10:49 +0530

    Add files via upload

---
+++ routes/todos.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+  router = express.Router(),
+  db = require("../models"),
+  helpers = require("../helpers/todos");
+
+router.route("/").get(helpers.getTodos).post(helpers.createTodo);
+router
+  .route("/:todoId")
+  .get(helpers.getTodo)
+  .put(helpers.updateTodo)
+  .delete(helpers.deleteTodo);
+
+module.exports = router;
